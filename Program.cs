a86965d087498b2b67e944fdcfbd12727054a605
Mohammed Abdullah <caladhary@gmail.com>
2023-07-17 16:26:20 +0400

    inOrder,postOrder and preOrder traversal of BST

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+namespace BinarySearchTree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            BinarySearchTree bst = new BinarySearchTree();
+
+            bst.Insert(90);
+            bst.Insert(30);
+            bst.Insert(50);
+            bst.Insert(40);
+            bst.Insert(10);
+            bst.Insert(60);
+            bst.Insert(20);
+
+            Console.WriteLine("\nIn-order traversal of the BST:");
+            bst.InOrderTraversal();
+
+            Console.WriteLine("\nPost-order traversal of the BST:");
+            bst.TraversePostOrder();
+
+            Console.WriteLine("\nPre-order traversal of the BST:");
+            bst.TraversePreOrder();
+        }
+    }
+}
---
+++ binarySearchTree.cs
@@ -0,0 +1,85 @@
+﻿using System;
+namespace BinarySearchTree
+{
+    internal class BinarySearchTree
+    {
+        private Node root;
+
+        public BinarySearchTree()
+        {
+            root = null;
+        }
+        public class Node
+        {
+            public int data;
+            public Node left;
+            public Node right;
+
+            public Node(int item)
+            {
+                data = item;
+                left = right = null;
+            }
+        }
+        public void Insert(int data)
+        {
+            root = InsertRec(root, data);
+        }
+
+        private Node InsertRec(Node root, int data)
+        {
+            if (root == null)
+            {
+                root = new Node(data);
+                return root;
+            }
+
+            if (data < root.data)
+                root.left = InsertRec(root.left, data);
+            else if (data > root.data)
+                root.right = InsertRec(root.right, data);
+
+            return root;
+        }
+        public void InOrderTraversal()
+        {
+            InOrderTraversalBst(root);
+        }
+
+        private void InOrderTraversalBst(Node root)
+        {
+            if (root != null)
+            {
+                InOrderTraversalBst(root.left);
+                Console.Write(root.data + " ");
+                InOrderTraversalBst(root.right);
+            }
+        }
+        public void TraversePostOrder()
+        {
+            TraversePostOrderBst(root);
+        }
+        private void TraversePostOrderBst(Node root)
+        {
+            if (root != null)
+            {
+                TraversePostOrderBst(root.left);
+                TraversePostOrderBst(root.right);
+                Console.Write(root.data + " ");
+            }
+        }
+        public void TraversePreOrder()
+        {
+            TraversePreOrderBst(root);
+        }
+        private void TraversePreOrderBst(Node root)
+        {
+            if (root != null)
+            {
+                Console.Write(root.data + " ");
+                TraversePreOrderBst(root.left);
+                TraversePreOrderBst(root.right);
+            }
+        }
+    }
+}
